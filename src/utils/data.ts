a687f3dc044c75d169c613a296d253216b35f587
lyswhut <lyswhut@qq.com>
2023-11-08 13:49:10 +0800

    自定义源变更
    (cherry picked from commit 2d184cb371fa2970742562d8a84ff672f4ff7cda)

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -479,6 +479,35 @@ export const getUserApiScript = async(id: string): Promise<string> => {
   return script
 }
 
+const INFO_NAMES = {
+  name: 24,
+  description: 36,
+  author: 56,
+  homepage: 1024,
+  version: 36,
+} as const
+type INFO_NAMES_Type = typeof INFO_NAMES
+const matchInfo = (scriptInfo: string) => {
+  const infoArr = scriptInfo.split(/\r?\n/)
+  const rxp = /^\s?\*\s?@(\w+)\s(.+)$/
+  const infos: Partial<Record<keyof typeof INFO_NAMES, string>> = {}
+  for (const info of infoArr) {
+    const result = rxp.exec(info)
+    if (!result) continue
+    const key = result[1] as keyof typeof INFO_NAMES
+    if (INFO_NAMES[key] == null) continue
+    infos[key] = result[2].trim()
+  }
+
+  for (const [key, len] of Object.entries(INFO_NAMES) as Array<{ [K in keyof INFO_NAMES_Type]: [K, INFO_NAMES_Type[K]] }[keyof INFO_NAMES_Type]>) {
+    infos[key] ||= ''
+    if (infos[key] == null) infos[key] = ''
+    else if (infos[key]!.length > len) infos[key] = infos[key]!.substring(0, len) + '...'
+  }
+
+  return infos as Record<keyof typeof INFO_NAMES, string>
+}
+
 export const parseUserScript = async(script: string): Promise<LX.UserApi.UserApiInfo> =>{
   let scriptInfo = script.split(/\r?\n/)
   let name = scriptInfo[1] || ''
@@ -496,15 +525,24 @@ export const parseUserScript = async(script: string): Promise<LX.UserApi.UserApi
   }
 }
 export const addUserApi = async(script: string): Promise<LX.UserApi.UserApiInfo> => {
-  const apiInfo = await parseUserScript(script)
-  const isRepeat = !!(userApis.find(item=> item.name === apiInfo.name))
-  if(!isRepeat){
-    userApis.push(apiInfo)
-    await saveDataMultiple([
-      [userApiPrefix, userApis],
-      [`${userApiPrefix}${apiInfo.id}`, script],
-    ])
+  const result = /^\/\*[\S|\s]+?\*\//.exec(script)
+  if (!result) throw new Error('无效的自定义源文件')
+
+  let scriptInfo = matchInfo(result[0])
+
+  scriptInfo.name ||= `user_api_${new Date().toLocaleString()}`
+  const apiInfo = {
+    id: `user_api_${Math.random().toString().substring(2, 5)}_${Date.now()}`,
+    ...scriptInfo,
+    script,
+    allowShowUpdateAlert: true,
   }
+  // const isRepeat = !!(userApis.find(item=> item.name === apiInfo.name))
+  userApis.push(apiInfo)
+  await saveDataMultiple([
+    [userApiPrefix, userApis],
+    [`${userApiPrefix}${apiInfo.id}`, script],
+  ])
   return apiInfo
 }
 export const removeUserApi = async(ids: string[]) => {
